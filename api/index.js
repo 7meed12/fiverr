eeecfe32682e2652b7f772953955a958398a252a
7meed <abdulhameed.khaled@everestminds.com>
2023-03-26 21:46:32 +0200

    add cors to application

--- api/index.js
+++ api/index.js
@@ -8,9 +8,11 @@ import conversationRoute from './routes/conversation.route.js';
 import messageRoute from './routes/message.route.js';
 import authRoute from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
-app.use(express.json()); 
+app.use(cors({origin :"http://localhost:5173", credentials : true})) 
+app.use(express.json());
 app.use(cookieParser()); 
 dotenv.config();
 mongoose.set("strictQuery" , true);
