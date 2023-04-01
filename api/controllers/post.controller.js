b596fb214304b59cac2245a5f4da4a00e8566b81
7meed <abdulhameed.khaled@everestminds.com>
2023-04-01 18:58:52 +0200

    add functionaliy to posts controller

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -17,20 +17,39 @@ export const deletePost = async (req , res , next) => {
     try{
         const {id} = req.params;
         const post = await Post.findById(id);
+         if(!post){
+            throw createError(404 , "Post not found")
+        }
         if(post.userId != req.userId){
             throw createError(403 , "You are not authorized to delete this post")
         }
-        if(!post){
-            throw createError(404 , "Post not found")
-        }
+       
         await Post.findByIdAndDelete(id);
         res.status(200).json({message : "Post deleted successfully"})
         
     }catch(error) {next(error)}
 }
-export const getPost = (req , res , next) => {
-    try{}catch(error) {next(error)}
+export const getPost =async (req , res , next) => {
+    try{
+        const {id} = req.params;
+        const post = await Post.findById(id);
+        if(!post){
+            throw createError(404 , "Post not found")
+        }
+        res.status(200).json(post)
+
+    }catch(error) {next(error)}
 }
-export const getPosts = (req , res , next) => {
-    try{}catch(error) {next(error)}
+export const getPosts = async(req , res , next) => {
+    const query = req.query;
+    const filters = {
+        ...(query.category && {category : query.category} ), 
+        ...(query.userId && {userId : query.userId} ), 
+        ...(query.search && {title :{$regex : query.search , $option : "i"}} )
+    }
+    try{
+        const posts = await Post.find(filters);
+        res.status(200).json(posts)
+
+    }catch(error) {next(error)}
 }
