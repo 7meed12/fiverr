c9f4e13df0a34c6a26af91e8e6dde0f7b6465d9b
7meed <abdulhameed.khaled@everestminds.com>
2023-05-21 21:06:56 +0300

    remove unwanted routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -45,34 +45,7 @@ function App() {
           path: "/",
           element: <Home />,
         },
-        {
-          path: "/gigs",
-          element: <Gigs />,
-        },
-        {
-          path: "/myGigs",
-          element: <MyGigs />,
-        },
-        {
-          path: "/orders",
-          element: <Orders />,
-        },
-        {
-          path: "/messages",
-          element: <Messages />,
-        },
-        {
-          path: "/message/:id",
-          element: <Message />,
-        },
-        {
-          path: "/add",
-          element: <Add />,
-        },
-        {
-          path: "/gig/:id",
-          element: <Gig />,
-        },
+       
         {
           path: "/register",
           element: <Register />,
