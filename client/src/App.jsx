c3f5ad49da83b8e244da7ac03b28f632ebc24ac4
7meed <abdulhameed.khaled@everestminds.com>
2023-05-21 21:11:20 +0300

    remove

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,8 @@ import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
-import Gigs from "./pages/gigs/Gigs";
-import Gig from "./pages/gig/Gig";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import Add from "./pages/add/Add";
-import Orders from "./pages/orders/Orders";
-import Messages from "./pages/messages/Messages";
-import Message from "./pages/message/Message";
-import MyGigs from "./pages/myGigs/MyGigs";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import {
